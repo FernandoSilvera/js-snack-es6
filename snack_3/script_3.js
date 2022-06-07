81ee593d1c572b4e0c75e60b68e706f5a3b04a6e
FernandoSilvera <fernando.silvera02@gmail.com>
2022-06-07 17:57:05 +0200

    Snack 3 and 4 bootstrap.

---
+++ snack_3/script_3.js
@@ -0,0 +1,14 @@
+// Dato l’array di nomi const myArray = [‘Michele’, ‘Fabio’, ‘Roberto’, ‘Giovanni’, ‘Simone’, ‘Chiara’];
+// Dati due numeri min e max (min più piccolo di max).
+// Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
+// Eseguiamo questo esercizio prima con forEach e poi con filter.
+
+
+const names = ["Michele", "Fabio", "Roberto", "Giovanni", "Simone", "Chiara"];
+const min = 1;
+const max = 4;
+
+// forEach
+names.forEach((name, i, names) => {
+  
+});
